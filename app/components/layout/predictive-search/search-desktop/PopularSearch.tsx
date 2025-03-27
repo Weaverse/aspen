5dff6d5802fe68669bb6c5c6b9aa14acb5e4670f
dangthang <dangthangkk2000@gmail.com>
2025-03-27 16:27:49 +0700

    Add PopularSearch component and refactor predictive search layout for both mobile and desktop, enhancing usability and results display

---
+++ app/components/layout/predictive-search/search-desktop/PopularSearch.tsx
@@ -0,0 +1,31 @@
+import { Link } from "@remix-run/react";
+
+export function PopularSearch() {
+  let popularSearches = [
+    "chair",
+    "barrel chair",
+    "play chair",
+    "ourf chair",
+    "balcony chair",
+  ];
+
+  return (
+    <div className="flex flex-col gap-4 max-w-page mx-auto">
+      <h3 className="text-sm font-semibold uppercase">
+        Popular Searches
+      </h3>
+      <ul className="flex flex-col gap-2">
+        {popularSearches.map((search, index) => (
+          <li key={index}>
+            <Link
+              to={`/search?q=${encodeURIComponent(search)}`}
+              className="block transition-transform duration-200 hover:-translate-y-1 w-fit"
+            >
+                <span className="text-sm">{search}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
