d1f554e8bd505fd18ba19b16522d1997a1439498
Hieu1866 <hieuwv0091@gmail.com>
2026-01-18 19:56:43 +0700

    update grid layout to 3 columns on tablet

--- app/sections/videos/items.tsx
+++ app/sections/videos/items.tsx
@@ -32,7 +32,7 @@ let VideoItems = forwardRef<HTMLElement, VideoItemsProps>((props, ref) => {
       {/* Desktop view */}
       <div
         ref={ref as any}
-        className="hidden md:grid md:grid-cols-2 lg:grid-cols-3"
+        className="hidden md:grid md:grid-cols-3"
         style={{ gap, ...style }}
       >
         {children}
