f6e4f5df5ac872f67704175609ab3ef1c1c19a3c
Hieu1866 <hieuwv0091@gmail.com>
2026-01-22 09:12:31 +0700

    Configure cloudflare workers deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import { hydrogen } from "@shopify/hydrogen/vite";
-import { oxygen } from "@shopify/mini-oxygen/vite";
+// import { oxygen } from "@shopify/mini-oxygen/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -8,7 +9,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     hydrogen(),
-    oxygen(),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    // oxygen(),
     reactRouter(),
     tsconfigPaths(),
     tailwindcss(),
